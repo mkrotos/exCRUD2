bedd3e13196dd72602f43b3b364a65ec47d37703
Mariusz Krotos <mar.krotos@gmail.com>
2018-09-18 14:27:39 +0200

    employee dao init

---
+++ src/main/Java/EmployeeDAO.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    public void addEmployee(Employee employee);
+
+    public List<Employee> getAllEmployees();
+
+    public void deleteEmployee(Integer employeeId);
+
+    public Employee updateEmployee(Employee employee);
+
+    public Employee getEmployee(int employeeId);
+}
---
+++ src/main/Java/EmployeeDaoImpl.java
@@ -0,0 +1,31 @@
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class EmployeeDaoImpl implements EmployeeDAO {
+    @Override
+    public void addEmployee(Employee employee) {
+
+    }
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return null;
+    }
+
+    @Override
+    public void deleteEmployee(Integer employeeId) {
+
+    }
+
+    @Override
+    public Employee updateEmployee(Employee employee) {
+        return null;
+    }
+
+    @Override
+    public Employee getEmployee(int employeeId) {
+        return null;
+    }
+}
